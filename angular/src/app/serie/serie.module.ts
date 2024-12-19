57c47dfe4adda9109846309e8ec1ab15dc45dcb9
6ena <gena.gra.agui@gmail.com>
2024-12-19 02:00:36 -0300

    Se creó el modulo de serie en angular

---
+++ angular/src/app/serie/serie.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SeriesComponent } from './series/series.component';
+
+
+
+@NgModule({
+  declarations: [SeriesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SerieModule { }
